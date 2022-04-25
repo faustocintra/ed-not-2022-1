1de0ae9cd799bcb81a9b04168b96005e3c560f9d
Fausto G. Cintra <professor@faustocintra.com.br>
2022-04-25 20:29:12 -0300

    (25/04) Correção da PROVA 1 e início da estrutura pilha

---
+++ lib/stack.py
@@ -0,0 +1,8 @@
+"""
+    Classe que implementa a estrutura de dados PILHA
+"""
+class Stack:
+
+    """ Método construtor """
+    def __init__(self):
+        self.__stack = []   # Inicializa uma lista vazia
